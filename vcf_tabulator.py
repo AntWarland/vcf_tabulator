8ce333b382cde12a3cc8bf6e0ac5f5966996ee5d
AntWarland <ant.warland@gmail.com>
2025-01-15 09:11:40 +0000

    working but slow

--- vcf_tabulator.py
+++ vcf_tabulator.py
@@ -8,6 +8,8 @@ import pandas as pd
 from funcs import tabulate_vcf, summarise_variants
 # from pysam import VariantFile
 
+pd.set_option('display.max_rows', 500)
+
 parser = argparse.ArgumentParser(description='Find UPD events in NGS Trio Data')
 parser.add_argument('--vcf', type=str, required=True,
 				help='The path to the VCF file. Must be bgzipped and tabixed.')
@@ -16,8 +18,10 @@ args = parser.parse_args()
 
 vcf = args.vcf
 
+pd.set_option('display.max_rows', 500)
 df = tabulate_vcf(vcf)
-# print(df.head())
-# print(df.tail())
+df.to_csv("./df_all_variants_binaries.csv") 
+print("dataframe saved to file.")
+
 
 # summary_df = summarise_variants(df)
